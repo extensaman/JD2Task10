1906d354af5f37828c0d41cb2cbe642a016c6658
extensaman <you@example.comverus.wedding@gmail.com>
2022-05-14 12:17:07 +0300

    logger changed to LOGGER

--- controller/src/main/java/by/it/academy/controller/command/impl/TaskCommand.java
+++ controller/src/main/java/by/it/academy/controller/command/impl/TaskCommand.java
@@ -2,11 +2,6 @@ package by.it.academy.controller.command.impl;
 
 import by.it.academy.controller.command.Command;
 import by.it.academy.controller.command.Constant;
-import by.it.academy.services.ServiceProvider;
-import by.it.academy.services.StudentService;
-import by.it.academy.services.TaskService;
-import by.it.academy.services.dto.StudentDto;
-import by.it.academy.services.dto.TaskDto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,17 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class TaskCommand implements Command {
-    private static final Logger logger = LogManager.getLogger(TaskCommand.class);
-    public static final String TASKS = "tasks";
-    private final TaskService taskService = ServiceProvider.getInstance().taskService();
+    private static final Logger LOGGER = LogManager.getLogger(TaskCommand.class);
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        logger.trace(getClass().getSimpleName());
-        List<TaskDto> allTask = taskService.taskDtos();
-        req.getSession().setAttribute(TASKS, allTask);
+        LOGGER.trace(getClass().getSimpleName());
         req.getRequestDispatcher(Constant.TEMPLATE_PAGE).forward(req,resp);
     }
 }
