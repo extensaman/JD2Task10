51ce971a51af0559924e4c8e6be7fd163e91e858
Safer68 <Safer@yandex.ru>
2022-05-14 21:24:45 +0300

    сделал task&assessment но пока предварительно

---
+++ service/src/main/java/by/it/academy/services/dto/AssessmentTdo.java
@@ -0,0 +1,19 @@
+package by.it.academy.services.dto;
+
+import by.it.academy.repository.entity.Assessment;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+public class AssessmentTdo {
+    private final Integer id;
+    private String name;
+
+    public AssessmentTdo(Assessment assessment) {
+        this.id = assessment.getId();
+        this.name = assessment.getFeedback();
+    }
+}
