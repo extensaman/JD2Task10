59ed3911823cf57c525fd6014552f889c53875eb
Alexander Grigorovich <algrigr@gmail.com>
2022-05-17 01:18:27 +0300

    service

---
+++ service/src/main/java/by/it/academy/services/dto/AdminMentorDto.java
@@ -0,0 +1,22 @@
+package by.it.academy.services.dto;
+
+import by.it.academy.repository.entity.Course;
+import by.it.academy.repository.entity.Mentor;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+
+public class AdminMentorDto {
+    private Integer id;
+    private String nameMentor;
+
+    public AdminMentorDto(Mentor mentor) {
+        this.id = mentor.getId();
+        this.nameMentor = mentor.getMentorName();
+    }
+}
+
