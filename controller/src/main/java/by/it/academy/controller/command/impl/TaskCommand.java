aab76cf968d1a040c9733b480b7fb0406978fb94
Alexander Grigorovich <algrigr@gmail.com>
2022-05-17 01:06:09 +0300

    daoImpl for Mentor

--- controller/src/main/java/by/it/academy/controller/command/impl/TaskCommand.java
+++ controller/src/main/java/by/it/academy/controller/command/impl/TaskCommand.java
@@ -15,12 +15,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class TaskCommand implements Command {
-    private static final Logger logger = LogManager.getLogger(TaskCommand.class);
+    private static final Logger LOGGER = LogManager.getLogger(TaskCommand.class);
     public static final String TASKS = "tasks";
     private final TaskService taskService = ServiceProvider.getInstance().taskService();
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        logger.trace(getClass().getSimpleName());
+        LOGGER.trace(getClass().getSimpleName());
         List<TaskDto> allTask = taskService.findAllTaskDto();
         req.getSession().setAttribute(TASKS, allTask);
         req.getRequestDispatcher(Constant.TEMPLATE_PAGE).forward(req,resp);
--- repository/src/main/java/by/it/academy/repository/dao/AdminDao.java
+++ repository/src/main/java/by/it/academy/repository/dao/AdminDao.java
@@ -2,11 +2,14 @@ package by.it.academy.repository.dao;
 
 import by.it.academy.repository.entity.Admin;
 import by.it.academy.repository.entity.Course;
+import by.it.academy.repository.entity.Mentor;
 
 import java.util.List;
 
 public interface AdminDao extends EntityDao<Admin> {
 
     List<Course> showAllCourseAdmin(Integer adminId);
+
+    List<Mentor> showAllMentorAdmin(Integer mentorId);
 
 }
