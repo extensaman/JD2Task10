98a44200fbf4248c3c063999d40e2bb431c66ce7
Safer68 <Safer@yandex.ru>
2022-05-17 18:40:47 +0300

    сделал удаление

--- service/src/main/java/by/it/academy/services/impl/TaskServiceImpl.java
+++ service/src/main/java/by/it/academy/services/impl/TaskServiceImpl.java
@@ -50,9 +50,15 @@ public class TaskServiceImpl implements TaskService {
         taskDao.save(task);
         taskDao.closeDao();
     }
+    public void removeTask(Integer taskId){
+        TaskDao taskDao = DaoProvider.getInstance().getTaskDao();
+        taskDao.delete(taskId);
+        taskDao.closeDao();
+    }
 
     public static void main(String[] args) {
-        new TaskServiceImpl().createTask("kksdkfs",1);
+        new TaskServiceImpl().removeTask(6);
+        /*new TaskServiceImpl().createTask("kksdkfs",1);*/
        /*System.out.println(new TaskServiceImpl().findAllTaskDto());*/
         /* System.out.println(new TaskServiceImpl().getListOfTaskAssessment(3));*/
     }
